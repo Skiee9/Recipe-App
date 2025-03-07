dacce5edd1ffc7a70bef11d5079b9974f8b35dc3
skiee9 <chaudharyayushi785@gmail.com>
2025-03-07 21:24:06 +0530

    added the links in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
+    // navbar started here
     <nav>
-     <Link to="/">Home</Link>
-     <Link to="/about">About</Link>
+
+
+    <div>
+      <Link to="/home">logo</Link>
+    </div>
+
+    
+    {/* middle navbar */}
+    <div className='center-nav'>  
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    </div>
+   
      <Link to="/login">Login</Link>
     </nav>
   )
