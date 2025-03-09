aa180505a68198bfc7974aa07099367e9e3f4fa6
skiee9 <chaudharyayushi785@gmail.com>
2025-03-09 23:48:15 +0530

    done with the coding part now the styling is left

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -6,6 +6,8 @@ const RecipePage = () => {
   return (
     <div className="recipe-page">
       <div className="recipe-container">
+      <h1>hy</h1>
+
         <RecipeDetail />
       </div>
     </div>
