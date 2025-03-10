84a802b15498a698be288bc36a2237463e97d252
skiee9 <chaudharyayushi785@gmail.com>
2025-03-10 15:18:29 +0530

    adding the sidebar functionality

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -6,7 +6,7 @@ const RecipePage = () => {
   return (
     <div className="recipe-page">
       <div className="recipe-container">
-      <h1>hy</h1>
+      
 
         <RecipeDetail />
       </div>
